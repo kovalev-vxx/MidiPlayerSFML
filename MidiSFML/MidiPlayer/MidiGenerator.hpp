398a9e7a908b0fdd7ca8badb4f641f9344bb60d0
kovalev-vxx <kovalev.vxx@gmail.com>
2022-05-15 19:01:02 +0300

    new branch

--- MidiSFML/MidiPlayer/MidiGenerator.hpp
+++ MidiSFML/MidiPlayer/MidiGenerator.hpp
@@ -1,11 +1,3 @@
-//
-//  MidiGenerator.hpp
-//  MidiSFML
-//
-//  Created by Valery Kovalev on 15.05.2022.
-//  Copyright © 2022 Valery Kovalev. All rights reserved.
-//
-
 #ifndef MidiGenerator_hpp
 #define MidiGenerator_hpp
 
