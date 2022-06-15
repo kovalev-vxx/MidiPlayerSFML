a90705302a22a893fca63942ae044eb56e560a0b
EgorChernov5 <EgorChernov3140@gmail.com>
2022-06-15 12:32:46 +0300

    Git clone

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/purpleduck/CLionProjects/MidiPlayerSFML
+# Install script for directory: /home/purpleduck/CLionProjects/MidiPlayerSFML2
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/purpleduck/CLionProjects/MidiPlayerSFML/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/purpleduck/CLionProjects/MidiPlayerSFML2/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
